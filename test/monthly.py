d428b4cc1c3b80e78ccc44e8bf4aa724b8b78fd1
ElieBonin <elie.bonin@gmail.com>
2022-01-16 18:09:01 +0000

    fix

--- test/monthly.py
+++ test/monthly.py
@@ -1,22 +1,17 @@
 from binance_public.handlers.handler import Binance
 
-trading_type = None
-symbols = None
-num_symbols = 1
-# dates = ['2022-01-14']
-dates = None
+symbols = ['BTCUSDT', 'ETHUSDT']
 st_dt = '2022-01-13'
 ed_dt = '2022-01-14'
 folder = None
-checksum = 1
 
-bnb = Binance(data_type='aggTrades', symbols=['BTCUSDT', 'ETHUSDT'], start_date=st_dt, end_date=ed_dt)
+bnb = Binance(data_type='aggTrades', symbols=symbols, start_date=st_dt, end_date=ed_dt, folder=folder)
 bnb.get_monthly_data(unzip=False, keep_unzip=False)
 
-bnb = Binance(data_type='klines', symbols=['BTCUSDT', 'ETHUSDT'], start_date=st_dt, end_date=ed_dt)
+bnb = Binance(data_type='klines', symbols=symbols, start_date=st_dt, end_date=ed_dt, folder=folder)
 bnb.get_monthly_data(unzip=False, keep_unzip=False)
 
-bnb = Binance(data_type='trades', symbols=['BTCUSDT', 'ETHUSDT'], start_date=st_dt, end_date=ed_dt)
+bnb = Binance(data_type='trades', symbols=symbols, start_date=st_dt, end_date=ed_dt, folder=folder)
 bnb.get_monthly_data(unzip=False, keep_unzip=False)
 
 print()
